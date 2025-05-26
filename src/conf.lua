5c6cfe7fe5a5be2316d8bde38bf8977f2867a07b
SomeRandomGuy <faizan163123@gmail.com>
2025-05-26 20:54:16 +0100

    init commit

---
+++ src/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.identity = "../data/saves"
+    t.console = false
+    t.gammacorrect = true
+    t.window.title = "Metroidvania Month 28"
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.setVSync = 0
+end
